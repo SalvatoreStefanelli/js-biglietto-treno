429a88fba7652e5b76f627d0e693485c03c336d8
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2023-11-18 15:18:35 +0100

    add variables and console.log(price)

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,4 +1,23 @@
 const chilometers = prompt('Quanti km vuoi percorrere?');
+console.log('chilometers');
 
-const age = prompt('Quanti anni hai?');
+// const age = prompt('Quanti anni hai?');
 
+// const priceKm = 0.21;
+
+// const price = priceKm * chilometers;
+
+// console.log('price');
+
+// if (age < 18) {
+   
+//     (price - 20%);
+// }
+
+// if (age > 65) { 
+    
+//     price - 40%
+
+// } 
+
+// console.log('price');
