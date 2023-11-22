ea98faa0a42676d312599d12c2e0caaace830c44
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2023-11-22 20:02:27 +0100

    correct ex

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -20,24 +20,21 @@ const priceSenior = price - saleSenior;
 // Calcolo del prezzo del biglietto scontato per i minorenni
 if (age < 18) {
     
-    console.log(priceYoung);
-    document.getElementById('total_price').innerHTML = priceYoung;
+    console.log(priceYoung.toFixed(2));
+    document.getElementById('total_price').innerHTML = priceYoung.toFixed(2);
     
 // Calcolo del prezzo del biglietto scontato per gli over 65
 } else if (age > 65) {
     
-    console.log(priceSenior);
-    document.getElementById('total_price').innerHTML = priceSenior;
+    console.log(priceSenior.toFixed(2));
+    document.getElementById('total_price').innerHTML = priceSenior.toFixed(2);
 
 // Calcolo del prezzo del biglietto tra i 18 e i 65 anni
 } else {
     
-    console.log(price);
-    document.getElementById('total_price').innerHTML = price;
+    console.log(price.toFixed(2));
+    document.getElementById('total_price').innerHTML = price.toFixed(2);
 }
 
-console.log(price.toFixed(2));
-console.log(priceYoung.toFixed(2));
-console.log(priceSenior.toFixed(2));
 
 
